d81c752a93fc27309ef6c6d7602a22ab1ea3caaf
LeRedBunny <alexi.toutou22@gmail.com>
2026-04-05 19:28:56 +0200

    Delete pages_php/plats directory

--- pages_php/plats/coeur.php
+++
@@ -1,8 +0,0 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-    <?php
-        $_SESSION["plat"]="Coeur de corail";
-        header("Location: ../commander.php");
-    ?>
-</html>
--- pages_php/plats/glace.php
+++
@@ -1,8 +0,0 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-    <?php
-        $_SESSION["plat"]="Glace à l'émeraude";
-        header("Location: ../commander.php");
-    ?>
-</html>
