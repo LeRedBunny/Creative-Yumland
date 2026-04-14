9591dbf2bf87b35a4f9b328da29c16a19c6064bf
Kristal <gregoire.marie1@etu.cyu.fr>
2026-04-14 17:36:41 +0200

    Add files via upload

---
+++ projet/filtre.js
@@ -0,0 +1,7 @@
+//1)envoyer une requête pour un fichier php de filtre
+
+//2)dans ce fichier, mettre le mot de filtre en minuscule, créer un tableau qui contient seulement les plats contenant les mots du filtre (voir l'exemple du prof)
+
+//3)ecrire dans le conteneur de plats tous les plats à partir du tableau reçu (en JS!)
+
+//4)should be all?
