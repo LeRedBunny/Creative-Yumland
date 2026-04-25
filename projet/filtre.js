c0d2f825b293b66f352932864da229e3c675fdc7
Kristal <gregoire.marie1@etu.cyu.fr>
2026-04-25 17:48:43 +0200

    Add files via upload

--- projet/filtre.js
+++ projet/filtre.js
@@ -1,6 +1,8 @@
 //1)envoyer une requête pour un fichier php de filtre
+//mettre à jour le début de carte.php
 
 //2)dans ce fichier, mettre le mot de filtre en minuscule, créer un tableau qui contient seulement les plats contenant les mots du filtre (voir l'exemple du prof)
+//fait à 75%
 
 //3)ecrire dans le conteneur de plats tous les plats à partir du tableau reçu (en JS!)
 
