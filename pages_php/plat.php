71c65c0b4ecca7a98d06d212adc2d6439fe6f89b
RedBunny <alexi.toutou22@gmail.com>
2026-05-04 00:43:52 +0200

    Update plat.php

--- pages_php/plat.php
+++ pages_php/plat.php
@@ -41,7 +41,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="../images/icon.png">
         <title> <?= $nomplat ?> </title>
-        <link rel="stylesheet" href="../css/style2.css">
+        <link rel="stylesheet" href="../css/style.css">
     </head>
     
     <body>
