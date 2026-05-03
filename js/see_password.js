b059576bc3a74a317797291e3fe869c1bdb9571b
RedBunny <alexi.toutou22@gmail.com>
2026-05-04 00:50:18 +0200

    Add files via upload

--- js/see_password.js
+++ js/see_password.js
@@ -5,5 +5,5 @@ function seePassword () {
     /* Toggles the password input from password to text */
     const TYPES = ['password', 'text'];
     let input = document.getElementById('password');
-    input.type = TYPES[(TYPES.indexOf(input.type) + 1) % 2];
+    input.type = TYPES[!TYPES.indexOf(input.type)];
 }
