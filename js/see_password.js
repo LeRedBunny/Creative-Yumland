02a673cf97427b43567523278dcfae3d8f016290
Kristal <gregoire.marie1@etu.cyu.fr>
2026-05-10 16:14:20 +0200

    Add files via upload

--- js/see_password.js
+++ js/see_password.js
@@ -5,5 +5,16 @@ function seePassword () {
     /* Toggles the password input from password to text */
     const TYPES = ['password', 'text'];
     let input = document.getElementById('password');
-    input.type = TYPES[!TYPES.indexOf(input.type)];
+    console.log('current type:' + input.type);
+    switch (input.type){
+        case 'password':
+            input.type=TYPES[1];
+            break;
+
+        case 'text':
+            input.type=TYPES[0];
+            break;
+    }
+    //input.type = TYPES[!TYPES.indexOf(input.type)];   for some reason, doesn't work alone, but reverts the action of the switch case when paired with it
+    console.log('type after change:' + input.type);
 }
