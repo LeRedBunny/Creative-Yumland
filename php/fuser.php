b0c018d7cf5231402357311ce315a5d40b2917a7
Kristal <gregoire.marie1@etu.cyu.fr>
2026-05-16 15:02:41 +0200

    Add files via upload

--- php/fuser.php
+++ php/fuser.php
@@ -6,35 +6,32 @@
     require('user_json.php');
 
 
-    //if($isset($_POST)){
+    $input=file_get_contents("php://input");    //on récupère le contenu du post/input 
 
-        $input=file_get_contents("php://input");    //on récupère le contenu du post
+    $user=json_decode($input,true); //on les traduit depuis json
 
-        $user=json_decode($input,true); //on les traduit depuis json
-    
-        //création de notre objet, à partir de l'input
-        //liste des paramètres pertinents (sécurité)
-        $datalist=array("name","firstname","email","tel","address","city","code","favorite_rock");
+    //création de notre objet, à partir de l'input
 
-        /*foreach($_POST as $index => $value){ echo '$_POST['.$index.']='.$value.'<br>'; } */
-        //pour chaque paramètre pertinent, $data stocke la même chose quue $user
-        foreach($datalist as $index => $value){
-            $data[$value]=$user[$value];
-            //echo '$data['.$value.']=$_POST['.$value.']='.$data[$value].'<br>';
-        }
+    //liste des paramètres pertinents (sécurité)
+    $datalist=array("name","firstname","email","tel","address","city","code","favorite_rock");
 
-        //récupération des données obsolètes de l'utilisateur (pour récupérer son ID)
-        $olduser=getUserFromEmail($data['email']);
-        $data['id']=$olduser["id"];
 
-        /*foreach($data as $index => $value){
-            echo 
-        }*/
-        $answer=updateUser($data);  //mise à jour utilisant la fonction de user_json.php
-        if($answer){
-            echo '1';
-        } else {
-            echo '0';
-        }
-    //}
+    //pour chaque paramètre pertinent, $data stocke la même chose que $user
+    foreach($datalist as $index => $value){
+        $data[$value]=$user[$value];
+        //echo '$data['.$value.']=$_POST['.$value.']='.$data[$value].'<br>';
+    }
+
+    //récupération des données obsolètes de l'utilisateur (pour récupérer son ID)
+    $olduser=getUserFromEmail($data['email']);
+    $data['id']=$olduser["id"];
+
+
+    $answer=updateUser($data);  //mise à jour utilisant la fonction de user_json.php
+    if($answer){
+        echo '1';
+    } else {
+        echo '0';
+    }
+
 ?>
