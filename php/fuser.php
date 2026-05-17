b57e1ed67276dfb68c96de9fa21f4a98155337e9
Kristal <gregoire.marie1@etu.cyu.fr>
2026-05-17 12:39:01 +0200

    Add files via upload

--- php/fuser.php
+++ php/fuser.php
@@ -5,33 +5,25 @@
     //usage des fonctions géniales d'Alexi
     require('user_json.php');
 
-
     $input=file_get_contents("php://input");    //on récupère le contenu du post/input 
 
-    $user=json_decode($input,true); //on les traduit depuis json
+    $data=json_decode($input,true); //on les traduit depuis json
 
-    //création de notre objet, à partir de l'input
-
-    //liste des paramètres pertinents (sécurité)
-    $datalist=array("name","firstname","email","tel","address","city","code","favorite_rock");
-
-
-    //pour chaque paramètre pertinent, $data stocke la même chose que $user
-    foreach($datalist as $index => $value){
-        $data[$value]=$user[$value];
-        //echo '$data['.$value.']=$_POST['.$value.']='.$data[$value].'<br>';
-    }
-
-    //récupération des données obsolètes de l'utilisateur (pour récupérer son ID)
-    $olduser=getUserFromEmail($data['email']);
-    $data['id']=$olduser["id"];
+    if(isset($data['email'])){  //sécurité
+        //récupération de l'id de l'utilisateur
+        $user=getUserFromEmail($data['email']);
+        $data['id']=$user["id"];
 
 
-    $answer=updateUser($data);  //mise à jour utilisant la fonction de user_json.php
-    if($answer){
-        echo '1';
-    } else {
+        $answer=updateUser($data);  //mise à jour utilisant la fonction de user_json.php
+        if($answer){
+            echo '1';
+        } else {
+            echo '0';
+        }
+    } else{
         echo '0';
     }
+    
 
 ?>
