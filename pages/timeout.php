893ae4c15f2b099e0043a6f0baf33301f20cdd4a
Kristal <gregoire.marie1@etu.cyu.fr>
2026-05-17 20:00:33 +0200

    Add files via upload

---
+++ pages/timeout.php
@@ -0,0 +1,16 @@
+<?php
+
+    require('../php/user_json.php');
+    require('../php/header.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php headLinks('TimeOut'); ?>
+    
+</head>
+<body id="prison">
+    
+</body>
+</html>
