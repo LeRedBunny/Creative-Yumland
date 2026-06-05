189c64f3dceb86db47ff18ec7c5a69c9f7a5a01a
Kristal <gregoire.marie1@etu.cyu.fr>
2026-06-05 18:23:59 +0200

    Add files via upload

--- php/checkform.php
+++ php/checkform.php
@@ -13,21 +13,21 @@
                 break;
             //la défense de connexion dépend beaucoup de l'intégrité d'inscription
             case 'inscription':
-
+                //echo 'checking for email<br>';
                 $array['email']=defaultvalue($array['email'],'error'); 
-
+                //echo 'checking for password<br>';
                 $array['password']=defaultvalue($array['password'],'error');
-                
+                //echo 'checking for firstname<br>';
                 $array['firstname']=defaultvalue($array['firstname'],'error');
-
+                //echo 'checking for name<br>';
                 $array['name']=defaultvalue($array['name'],'error');
-
+                //echo 'checking for tel<br>';
                 $array['tel']=defaultvalue($array['tel'],'error');
-
-                $array['address']=defaultvalue($array['adress'],'error');
-
+                //echo 'checking for address<br>/current value is'.$array['address'];
+                $array['address']=defaultvalue($array['address'],'error');
+                //echo 'checking for code<br>';
                 $array['code']=defaultvalue($array['code'],'error');
-
+                //echo 'checking for city<br>';
                 $array['city']=defaultvalue($array['city'],'error');
                 break;
             case 'commande':
@@ -45,7 +45,7 @@
             //on supprime tous ses espaces (il ne peut pas y en avoir), si elle est vide, on renvoie default
             $trimmed=str_replace(' ','',$data);
             if($trimmed==''){
-                //echo '$data is empty';
+                //echo $data.'is empty';
                 return $default;
             }//on la purge de ses caractères
             //echo 'no classic mistakes, replacing special characters<br>';
